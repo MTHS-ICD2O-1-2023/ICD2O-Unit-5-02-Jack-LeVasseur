88ab06fefa0dade7d71820fbbbc55f46480c9834
Jack LeVasseur <159185068+Jack-LeVasseur@users.noreply.github.com>
2024-04-25 18:24:57 +0000

    Update index.html

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Jack LeVasseur
+// Created on: April 24, 2024
 // This file contains the JS functions for index.html
+
+/**
+ * This function displays the slider value.
+ */
+function myButtonClicked() {
+  alwaysOnButtonChecked = document.getElementById('on-check').checked
+
+  if (alwaysOnButtonChecked == true) {
+    document.getElementById("radio-button-value").innerHTML =
+      "<p>The random number is: 10</p>"
+  } else {
+    document.getElementById("radio-button-value").innerHTML =
+      "<p>The random number is:-5</p>"
+  }
+}
