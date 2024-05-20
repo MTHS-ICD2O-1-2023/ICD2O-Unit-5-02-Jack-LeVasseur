3496c188de9b43dc86f7a8818195a53b6dd63692
Jack LeVasseur <159185068+Jack-LeVasseur@users.noreply.github.com>
2024-05-20 00:15:00 +0000

    Updated index.html

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@
 // Created on: April 24, 2024
 // This file contains the JS functions for index.html
 
+"Use strict"
+
 /**
  * This function displays the slider value.
  */
@@ -11,10 +13,16 @@ function myButtonClicked() {
   alwaysOnButtonChecked = document.getElementById('on-check').checked
 
   if (alwaysOnButtonChecked == true) {
+    let min = 1;
+    let max = 10;
+    let randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
     document.getElementById("radio-button-value").innerHTML =
-      "<p>The random number is: 10</p>"
+      "<p>The random number is:" + randomNum + "</p>"
   } else {
+    let min = -10;
+    let max = -1;
+    let randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
     document.getElementById("radio-button-value").innerHTML =
-      "<p>The random number is:-5</p>"
+      "<p>The random number is:" + randomNum + "</p>"
   }
 }
